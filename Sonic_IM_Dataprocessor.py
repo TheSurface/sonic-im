2ecb3718130073901354d4534b11a8396bca0be3
TheSurface <josiah.surface@gmail.com>
2022-01-11 15:11:50 -0500

    Add files via upload

--- Sonic_IM_Dataprocessor.py
+++ Sonic_IM_Dataprocessor.py
@@ -652,12 +652,12 @@ elif sonic_im_client == 'Cerebral':
         
         # Combine base and budget dataframes
         df_base_budget = pd.merge(df_base, df_budget_grouped, how='left', left_on=['date','Show Name'], right_on=['budget_spend_month','Show Name'])
-        
+        df_base_budget['Client Rate'].fillna(0,inplace=True)
 
 
 
         # Create lead and purchase calendar dataframes
-        df_final_output = pd.merge(df_base_budget, df_budget[['event_date','created_week','created_month','Show Name']],on=['Show Name'])
+        df_final_output = pd.merge(df_base_budget, df_budget[['event_date','created_week','created_month','Show Name']],how='left',left_on=['date'],right_on=['created_month'])
        
 
 
