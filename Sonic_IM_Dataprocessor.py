992fc41be9fb7b859279a907af563d2e9137c8d3
TheSurface <josiah.surface@gmail.com>
2022-01-12 11:09:36 -0500

    Add files via upload

--- Sonic_IM_Dataprocessor.py
+++ Sonic_IM_Dataprocessor.py
@@ -618,7 +618,7 @@ elif sonic_im_client == 'Cerebral':
             LEFT JOIN chartable_agg_df b ON a."Show Name" = b."Ad Campaign Name"
             
         WHERE 
-            (a."Actual Drop Day" <= "{cutoff_date}") OR
+            (a."Actual Drop Day" <= "{cutoff_date}")
             
             
         GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
